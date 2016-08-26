410f144483269c01e2642bfdf99e205674d1849a
Piotr Nosek <piotr.nosek@erlang-solutions.com>
2016-08-26 11:08:38 +0200

    Add main supervisor

--- mix.exs
+++ mix.exs
@@ -14,7 +14,8 @@ defmodule BuzzardServer.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger],
+     mod: {BuzzardServer, []}]
   end
 
   # Dependencies can be Hex packages:
