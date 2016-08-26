9d59c6a386f39aea7a17f83213d4c1b41f2ffa1f
Paweł Chrząszcz <pawel.chrzaszcz@erlang-solutions.com>
2016-08-26 15:46:19 +0200

    Return list of motion time periods

--- lib/motion_vod_handler.ex
+++ lib/motion_vod_handler.ex
@@ -6,7 +6,7 @@ defmodule :motion_vod_handler do
   end
 
   def handle(req, state) do
-    {:ok, req2} = :cowboy_req.reply(200, [], "test", req)
+    {:ok, req2} = :cowboy_req.reply(200, [], format_rows, req)
     {:ok, req2, state}
   end
 
@@ -14,4 +14,15 @@ defmodule :motion_vod_handler do
     :ok
   end
 
+  def format_rows() do
+    rows = IBuzzard.VideoSaver.read_all
+    struct = for {start, ent} <- rows do
+      {[{<<"start">>, DateTime.to_unix(start)} |
+         case ent do
+           :undefined -> [];
+           _ -> [{<<"end">>, DateTime.to_unix(ent)}]
+         end]}
+    end
+    :jiffy.encode(struct)
+  end
 end
